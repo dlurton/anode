d494757283f5378fca21445ee83b376b5b69832f
David Lurton <psydave@gmail.com>
2017-07-04 12:08:55 -0700

    Add REPL POC using linenoise-ng for command-line editing.

--- README.md
+++ README.md
@@ -12,9 +12,9 @@ The script `tools/build-all-dependencies` will build [ANTLR4](https://github.com
 
 You'll need git, [cmake](https://cmake.org/) (3.4.3 or later and gcc.  gcc 7.1.1 is known to work however earlier versions are likely to as well.
 
-To build ANTLR4's parser generator, [maven](https://maven.apache.org/what-is-maven.html) (version 3.5.0 is known to work) and Java 7 or later is also required.
+To build ANTLR4's parser generator [maven](https://maven.apache.org/what-is-maven.html) (version 3.5.0 is known to work) and Java 7 or later is also required.
 
-The script `tools/build-all-dependencies` will clone LLVM and ANTLR4's git repositories and build everything with all the neccessary options, placing all the source codes and intermediate files into `externs/scratch`.  This directory may be deleted to conserve disk space after everything has succesfully built, if desired.  If successful, the libraries and headers will be installed in `externs/llvm` and `externs/antlr4'.  These must exist prior to attempting to build LWNN itself.
+The script `tools/build-all-dependencies` will clone LLVM and ANTLR4's git repositories and build everything with all the neccessary options, placing all the source codes and intermediate files into `externs/scratch`.  This directory may be deleted to conserve disk space after everything has succesfully built, if desired.  If successful, the libraries and headers will be installed in `externs/llvm` and `externs/antlr4`.  These must exist prior to attempting to build LWNN itself.
 
 ### Building LWNN
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,82 @@
+#include <linenoise.h>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
 
 extern void do_antlr4_demo();
 extern void do_llvm_demo();
 
-int main() {
+
+
+static const char* examples[] = {
+        "db", "hello", "hallo", "hans", "hansekogge", "seamann", "quetzalcoatl", "quit", "power", NULL
+};
+
+void completionHook (char const* prefix, linenoiseCompletions* lc) {
+    size_t i;
+
+    for (i = 0;  examples[i] != NULL; ++i) {
+        if (strncmp(prefix, examples[i], strlen(prefix)) == 0) {
+            linenoiseAddCompletion(lc, examples[i]);
+        }
+    }
+}
+
+
+int main(int argc, char **argv) {
     do_antlr4_demo();
     do_llvm_demo();
+
+
+    linenoiseInstallWindowChangeHandler();
+
+    while(argc > 1) {
+        argc--;
+        argv++;
+        if (!strcmp(*argv, "--keycodes")) {
+            linenoisePrintKeyCodes();
+            exit(0);
+        }
+    }
+
+    const char* file = "~/.lwnn_history";
+
+    linenoiseHistoryLoad(file);
+    linenoiseSetCompletionCallback(completionHook);
+
+    printf("starting...\n");
+
+    char const* prompt = "\x1b[1;32mlwnn\x1b[0m> ";
+
+    bool keepGoing = true;
+    while (keepGoing) {
+        char* result = linenoise(prompt);
+
+        if (result == NULL) {
+            break;
+        } else if (!strncmp(result, "/history", 8)) {
+            /* Display the current history. */
+            for (int index = 0; ; ++index) {
+                char* hist = linenoiseHistoryLine(index);
+                if (hist == NULL) break;
+                printf("%4d: %s\n", index, hist);
+                free(hist);
+            }
+        }
+        if (*result == '\0') {
+            keepGoing = false;
+        } else {
+            printf("You said: %s\n", result);
+            linenoiseHistoryAdd(result);
+        }
+
+        free(result);
+        linenoiseHistorySave(file);
+    }
+
+    printf("Saving history...");
+    linenoiseHistoryFree();
+
+    return 0;
 }
+
