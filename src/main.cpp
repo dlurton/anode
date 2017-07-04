6be0e58a374203c7cd629b1af2a905d079661d40
David Lurton <psydave@gmail.com>
2017-07-04 12:34:10 -0700

    Adding top-level CMakeLists.txt and other minor adjustments.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include <linenoise.h>
+
 #include <cstring>
 #include <cstdlib>
 #include <cstdio>
 
 extern void do_antlr4_demo();
 extern void do_llvm_demo();
-
-
 
 static const char* examples[] = {
         "db", "hello", "hallo", "hans", "hansekogge", "seamann", "quetzalcoatl", "quit", "power", NULL
